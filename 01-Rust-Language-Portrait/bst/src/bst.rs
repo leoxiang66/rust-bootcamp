4ff3b65eea8d1d6e10807f0c0726c12c5389c284
benbencik <bencik42@gmail.com>
2024-12-08 19:25:56 +0100

    create a template file

--- 01-Rust-Language-Portrait/bst/src/bst.rs
+++ 01-Rust-Language-Portrait/bst/src/bst.rs
@@ -32,84 +32,22 @@ impl BST {
     }
 
     pub fn insert(&mut self, value: i32) {
-        self.root = Self::insert_node(self.root.take(), value);
-    }
-
-    fn insert_node(node: Option<Box<BSTNode>>, value: i32) -> Option<Box<BSTNode>> {
-        match node {
-            Some(mut current) => {
-                if value < current.value {
-                    current.left = Self::insert_node(current.left.take(), value);
-                } else if value > current.value {
-                    current.right = Self::insert_node(current.right.take(), value);
-                }
-                Some(current)
-            }
-            None => Some(Box::new(BSTNode::new(value))),
-        }
+        // TODO: 🦀Your task
     }
 
     pub fn search(&self, value: i32) -> bool {
-        Self::search_node(&self.root, value)
-    }
-
-    fn search_node(node: &Option<Box<BSTNode>>, value: i32) -> bool {
-        match node {
-            Some(current) => {
-                if value == current.value {
-                    true
-                } else if value < current.value {
-                    Self::search_node(&current.left, value)
-                } else {
-                    Self::search_node(&current.right, value)
-                }
-            }
-            None => false,
-        }
+        // TODO: 🦀Your task
     }
 
     pub fn min_value(&self) -> Option<i32> {
-        Self::find_min(&self.root)
-    }
-
-    fn find_min(node: &Option<Box<BSTNode>>) -> Option<i32> {
-        let mut current = node;
-        while let Some(n) = current {
-            if n.left.is_none() {
-                return Some(n.value);
-            }
-            current = &n.left;
-        }
-        None
+        // TODO: 🦀Your task
     }
 
     pub fn in_order(&self) -> Vec<i32> {
-        let mut result = Vec::new();
-        Self::in_order_traverse(&self.root, &mut result);
-        result
-    }
-
-    fn in_order_traverse(node: &Option<Box<BSTNode>>, result: &mut Vec<i32>) {
-        if let Some(current) = node {
-            Self::in_order_traverse(&current.left, result);
-            result.push(current.value);
-            Self::in_order_traverse(&current.right, result);
-        }
+        // TODO: 🦀Your task
     }
 
     pub fn height(&self) -> usize {
-        Self::calculate_height(&self.root)
-    }
-
-    fn calculate_height(node: &Option<Box<BSTNode>>) -> usize {
-        match node {
-            Some(current) => {
-                1 + std::cmp::max(
-                    Self::calculate_height(&current.left),
-                    Self::calculate_height(&current.right),
-                )
-            }
-            None => 0,
-        }
+        // TODO: 🦀Your task
     }
 }
