bb42d261cf116da39b4eabf4838208d94a5eb6db
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2025-05-12 17:58:21 +0800

    updates

--- 01-Rust-Language-Portrait/bst/src/bst.rs
+++ 01-Rust-Language-Portrait/bst/src/bst.rs
@@ -23,6 +23,21 @@ impl BST {
         BST { root: None }
     }
 
+    fn insert_node(node: Option<Box<BSTNode>>, value: i32) -> Option<Box<BSTNode>> {
+        match node {
+            Some(mut current) => {
+                if value <= current.value {
+                    current.left = Self::insert_node(current.left.take(), value);
+                } else {
+                    current.right = Self::insert_node(current.right.take(), value);
+                }
+                Some(current)
+            }
+
+            None => Some(Box::new(BSTNode::new(value))),
+        }
+    }
+
     pub fn from_vec(values: Vec<i32>) -> Self {
         let mut bst = BST::new();
         for value in values {
@@ -33,6 +48,7 @@ impl BST {
 
     pub fn insert(&mut self, value: i32) {
         // TODO: 🦀Your task
+        self.root = Self::insert_node(self.root.take(), value);
     }
 
     pub fn search(&self, value: i32) -> bool {
